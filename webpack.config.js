0846d1a59dda734c3067982219fda1aa81901666
bushra alam <siba742002@gmail.com>
2025-03-23 01:56:14 -0700

    installed webpack

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import path from 'path';
+
+export default {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: '[name].bundle.js',  // Fix filename conflict
+    path: path.resolve(process.cwd(), 'dist'),
+    clean: true,
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    open: true, // This will open the browser automatically
+    port: 8080,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  optimization: {
+    runtimeChunk: 'single',  // Creates a separate runtime bundle
+  },
+};
